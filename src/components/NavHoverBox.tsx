c27369658803584b0c001d4cc580b9c58f82e8e0
Comon <comon928@gmail.com>
2023-02-27 12:31:55 +0300

    Seven

--- src/components/NavHoverBox.tsx
+++ src/components/NavHoverBox.tsx
@@ -6,7 +6,7 @@ import {
     Icon
 } from '@chakra-ui/react'
 
-export default function NavHoverBox({ title, icon, description }: {title: string; description: string}) {
+export default function NavHoverBox({ title, icon, description }: {title: string; icon; description: string}) {
     return (
         <>
             <Flex
